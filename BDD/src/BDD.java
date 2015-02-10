7cb48ccbd056fe6f5ae40af92829cc5b2615fb35
Antoine Houdayer <antoine.houdayer@laposte.net>
2015-02-10 14:13:08 +0100

    Ajout du constructeur de la classe BDD, il se connecte à notre base de donnée

--- BDD/src/BDD.java
+++ BDD/src/BDD.java
@@ -1,7 +1,27 @@
+import java.sql.*;
+
+
+
 
 public class BDD implements BDDInterface
 {
+	Connection con ;
+    Statement st ;
+    ResultSet rs ;
+	public BDD()
+	{
+		 try {
+			 String url = "jdbc:mysql://localhost:3306/optibar"; // a changer
+		        String user = "admin"; // a changer 
+		        String password = "pact14"; //a changer
+	            con = DriverManager.getConnection(url, user, password);
+	            st = con.createStatement();
 
+	        } catch (Exception e) {
+	        	e.printStackTrace();
+	        }
+	}
+	
 	@Override
 	public boolean ajouterConsommation(int bluetoothID, int rFID, int volume) {
 		// TODO Auto-generated method stub
