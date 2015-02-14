6e48f74c0c62f1d53f18dd6cce75af2af5a57076
Yunzhi <baixiaobai001@gmail.com>
2015-02-14 20:33:40 +0100

    Finir les manipulations de base sur les tables.

--- src/BDD.java
+++ src/BDD.java
@@ -8,6 +8,7 @@ public class BDD implements BDDInterface
 	Connection con ;
     Statement st ;
     ResultSet rs ;
+    
 	public BDD(String dbname, String user, String mdp)
 	{
 		 try {
@@ -15,7 +16,7 @@ public class BDD implements BDDInterface
 	            con = DriverManager.getConnection("jdbc:mysql://localhost/"+dbname+"?" +
                         "user="+user+"&password="+mdp);
 	            st = con.createStatement();
-
+               System.out.println("yes");
 	        } catch (Exception e) {
 	        	e.printStackTrace();
 	        }
@@ -24,19 +25,27 @@ public class BDD implements BDDInterface
 	@Override
 	public boolean ajouterConsommation(int bluetoothID, int rFID, int volume) 
 	{
-		int cPK;
-		int codeBarre;
+		int cPK=0;
+		int codeBarre=0;
 		try
 		{
 			rs=st.executeQuery("SELECT CodeBarre FROM Associe WHERE Associe.BluetoothID="+bluetoothID);
-			rs.next();
+			while(rs.next())
+			{
 			codeBarre=rs.getInt(1);
+			}
 			rs=st.executeQuery("SELECT CPK FROM Barman WHERE Barman.CPK="+rFID);
-			rs.next();
+			while(rs.next())
+			{
 			cPK=rs.getInt(1);
-			st.execute("INSERT INTO Composition ("+codeBarre+","+volume+","+cPK+")");
+			}
+			if((codeBarre!=0)&&(cPK!=0))
+			{
+				st.executeUpdate("INSERT INTO Composition ("+codeBarre+","+volume+","+cPK+")");
+				//maque de méthode pour vérifier si bien Update.
+				System.out.println("Bien ajouter une consommation.");
+			}
 			//on met a jour le stock à la fin de la commande.
-			
 		}
 		catch(Exception e)
 		{
@@ -56,13 +65,119 @@ public class BDD implements BDDInterface
 	public boolean ajouterGoulot(int bluetooth) 
 	{
 		try {
-			st.execute("INSERT INTO Goulots (BluetoothID, EnCharge, NiveauDeCharge )" +
+			st.executeUpdate("INSERT INTO Goulots (BluetoothID, EnCharge, NiveauDeCharge )" +
 					"VALUES ('"+bluetooth+"','0','1.0')");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+			return false;
+		}return true;
+		
+	}
+	
+	public boolean modifierGoulot(int bluetooth,int enCharge, float niveauDeCharge)
+	{   
+		String updateSql = "UPDATE Goulots SET EnCharge="+enCharge+",NiveauDeCharge="
+				+ niveauDeCharge+ "WHERE BluetoothID ="+bluetooth;
+		try {
+			int updateResultat=st.executeUpdate(updateSql);
+			System.out.println("UPDATE:" + updateResultat);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+	}return true;
+	}
+	
+	public boolean supprimerGoulot(int bluetooth)
+	{
+		String sql = "DELETE FROM Goulots WHERE BluetoothID = "+bluetooth;
+		try
+		{
+		long deleteRes = st.executeUpdate(sql);
+		 System.out.print("DELETE:" + deleteRes);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+	}return true;
+	}
+
+	@Override
+	public boolean ajouterBarman(String rFID, String nom, String prenom,
+			int age, String dateEmbauche, int cPK) 
+	{
+		String sql = "INSERT INTO Barman (RFID, Nom, Prenom,Age, DateEmbauche,CPK)" +
+				"VALUES ('"+rFID+"','"+nom+"','"+prenom+"','"+age+"','"+dateEmbauche+"','"+cPK+"')";
+		try {
+			st.executeUpdate(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}return true;
+	}
+
+	@Override
+	public boolean modifierInformationBarman(String rFID, String nom,
+			String prenom, int age, String dateEmbauche, int cPK) 
+	{
+		String updateSql = "UPDATE Barman SET Nom="
+				+ nom+", Prenom="+prenom+",Age= "+age+",DateEmbauche=" +dateEmbauche+",CPK="+cPK+" WHERE RFID="+rFID;
+		try {
+		int updateResultat=st.executeUpdate(updateSql);
+		System.out.println("UPDATE:" + updateResultat);
+		
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
 		return false;
+}return true;
+
+	}
+
+	@Override
+	public boolean modifierCommandeDeBarman(String rFID, int cPK) {
+		String updateSql = "UPDATE Barman SET CPK="+cPK+" WHERE RFID="+rFID;
+		try {
+		int updateResultat=st.executeUpdate(updateSql);
+		System.out.println("UPDATE:" + updateResultat);
+		
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		return false;
+}return true;
+	}
+
+	@Override
+	public boolean supprimerBarman(String rFID) {
+		String sql = "DELETE FROM Barman WHERE RFID = "+rFID;
+		try
+		{
+		long deleteRes = st.executeUpdate(sql);
+		 System.out.print("DELETE:" + deleteRes);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+	}return true;
+	}
+
+	@Override
+	public boolean ajouterBoisson(int codeBarre, String nom, String marque,
+			int degre) 
+	{
+		String sql = "INSERT INTO Boisson (CodeBarre,Nom,Marque,Degre)" +
+				"VALUES ('"+codeBarre+"','"+nom+"','"+marque+"','"+degre+"')";
+		try {
+			st.executeUpdate(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}return true;
 	}
 
 }
--- src/test.java
+++ src/test.java
@@ -2,11 +2,18 @@
 public class test {
 
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
 BDDInterface bdd = new BDD("BAR", "root","12345678"); 
-bdd.ajouterGoulot(24687);
-
+/*bdd.ajouterGoulot(24688);
+bdd.modifierGoulot(24688,1,(float) 0.5);
+bdd.supprimerGoulot(24688);
+*/
+/*bdd.ajouterBarman("002", "Yunzhi", "MINAMI", 22, "2015.1.1", 0);
+//bdd.modifierInformationBarman("001", "Yunzhi2", "MINAMI2", 21, "2015.1.2", 1); avec problème inconnue.
+bdd.modifierCommandeDeBarman("001", 1);
+	*/
+bdd.ajouterBoisson(201201, "coca", "coca", 20);
 	}
-
 }
