2c447b1a07f73563cba1617278e33fead7e0f8ea
Aymon de Broglie <aymon.debroglie@telecom-paristech.fr>
2015-02-24 16:38:33 +0100

    Rajout du code pour une liaison serial

--- Bluetooth/src/MyDiscoveryListener.java
+++ Bluetooth/src/MyDiscoveryListener.java
@@ -1,11 +1,40 @@
 import javax.bluetooth.DeviceClass;
+import javax.bluetooth.DiscoveryAgent;
 import javax.bluetooth.DiscoveryListener;
+import javax.bluetooth.LocalDevice;
 import javax.bluetooth.RemoteDevice;
 import javax.bluetooth.ServiceRecord;
 
 
 public class MyDiscoveryListener implements DiscoveryListener{
+	
 	private static Object lock=new Object();
+	public static void main(String[] args) {
+		 try{
+		            // 1
+		            LocalDevice localDevice = LocalDevice.getLocalDevice();
+		 
+		            // 2
+		            DiscoveryAgent agent = localDevice.getDiscoveryAgent();
+		            
+		            // 3
+		            agent.startInquiry(DiscoveryAgent.GIAC, new MyDiscoveryListener());
+		            try {
+		                synchronized(lock){
+		                    lock.wait();
+		                }
+		            }
+		            catch (InterruptedException e) {
+		                e.printStackTrace();
+		            }
+		            System.out.println("Device Inquiry Completed. ");
+		             
+		        }
+		        catch (Exception e) {
+		            e.printStackTrace();
+		        }
+
+	}
     @Override
     public void deviceDiscovered(RemoteDevice btDevice, DeviceClass arg1) {
         String name;
