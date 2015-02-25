16b1e6d1ae1bfd2d68c2afe308181df51aec230b
Yunzhi <baixiaobai001@gmail.com>
2015-02-25 21:44:46 +0100

    merge into Achille

--- src/TestJena.java
+++ src/TestJena.java
@@ -1,17 +1,19 @@
-import java.io.IOException;  
-import java.io.InputStream;  
-import java.net.HttpURLConnection;  
-import java.net.URL;  
-  
-import com.hp.hpl.jena.ontology.OntModel;  
-import com.hp.hpl.jena.ontology.OntModelSpec;  
-import com.hp.hpl.jena.query.Query;  
-import com.hp.hpl.jena.query.QueryExecution;  
-import com.hp.hpl.jena.query.QueryExecutionFactory;  
-import com.hp.hpl.jena.query.QueryFactory;  
-import com.hp.hpl.jena.query.QuerySolution;  
-import com.hp.hpl.jena.query.ResultSet;  
-import com.hp.hpl.jena.rdf.model.ModelFactory;  
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+
   
 public class TestJena {  
   
